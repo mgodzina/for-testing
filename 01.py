65f2620f66154faca11dd72610dbedd13b95179d
Marcin <marcin@marcingodzina.pl>
2020-03-01 21:34:00 +0100

    sec

--- 01.py
+++ 01.py
@@ -1,10 +1,30 @@
+from selenium import webdriver
+#driver = webdriver.Chrome(executable_path=r'venv\\Scripts\\chromedriver.exe')
 
-test1 = 0 # todo other things
 
-test1 == 35
 
-st = str(56)
+# todo other things
 
-x = 0
 
-print(test1 + 'new')
+
+#driver.get('http://www.wp.pl')
+
+class Animal:
+    def __init__(self):
+        pass
+    def who(self):
+        print('who you')
+
+
+class Dog(Animal):
+    def __init__(self, name='azor', age=3):
+        self.name = name
+        self.age = age
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} {self.name} is {self.age} years old."
+
+azor = Dog(age='5')
+azor.who()
+print(azor.name)
+print(azor)
